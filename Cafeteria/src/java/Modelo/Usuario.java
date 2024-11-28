e4fc08534fb0bfc1dc99a35322397199b9e8520c
Paolo Crisóstomo <paolo_crisostomo@outlook.com>
2024-11-28 02:32:25 -0500

    ads
    asd

---
+++ Cafeteria/src/java/Modelo/Usuario.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Modelo;
+
+public class Usuario {
+    int id;
+    String Usuario;
+    String Contraseña;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String Usuario, String Contraseña) {
+        this.id = id;
+        this.Usuario = Usuario;
+        this.Contraseña = Contraseña;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(String Usuario) {
+        this.Usuario = Usuario;
+    }
+
+    public String getContraseña() {
+        return Contraseña;
+    }
+
+    public void setContraseña(String Contraseña) {
+        this.Contraseña = Contraseña;
+    }
+    
+    
+}
